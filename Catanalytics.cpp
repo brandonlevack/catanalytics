ca975b6e9a326d3df6168e9b42e762319711c5a2
brandonlevack <brandonlevack@hotmail.com>
2023-07-27 19:31:20 -0400

    Master branch first commit with comment Layout

---
+++ Catanalytics.cpp
@@ -0,0 +1,28 @@
+//#include <graphics.h>
+#include <iostream>
+using namespace std;
+
+
+//first stage (setup graphics window?)
+
+
+
+
+
+//take in / build board
+
+
+
+
+// turn by turn enter dice roll and trades
+
+
+
+
+//log in txt file or log file
+
+
+
+
+
+// after game graph results
