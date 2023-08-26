4359360b7f24de4e3b1c7c347abba72f68e960c4
brandonlevack <brandonlevack@hotmail.com>
2023-08-26 11:08:45 -0400

    1st stage of progress with hexagon classes near finished

--- GameTile.py
+++ GameTile.py
@@ -1,7 +1,14 @@
 from Hexagon import Hexagon
 
 class GameTile:
-    xCoord = 0
-    yCoord = 0
-    resource = ""
-    dieNumber = 0
+    def __init__ (self, x, y, resource, num, screen):
+        self.x = x
+        self.y = y
+        self.resource = resource
+        self.dieNumber = num
+        self.hex = Hexagon(x,y,screen)
+
+    def drawTile(self):
+        self.hex.drawHexagon()
+
+    
